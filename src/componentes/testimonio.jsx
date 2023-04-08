9143a23c8c3c30283749125c34309e235e7ffafd
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-04-07 21:43:29 -0500

    "ok"

--- src/componentes/testimonio.jsx
+++ src/componentes/testimonio.jsx
@@ -9,7 +9,7 @@ function Testimonio(props){
       alt="foto1"
       />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
+        <p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
         <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
         <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
